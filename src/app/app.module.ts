767995a6723d675d56cbe4b8e3315f8d781a90d7
Joao Artur <arturdligorio123@gmail.com>
2019-05-21 04:58:10 -0300

    calendar inserido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -12,15 +8,17 @@ import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AvisoComponent } from './aviso/aviso.component';
 import { MaterialDidaticoComponent } from './material-didatico/material-didatico.component';
-import { FlatpickrModule } from 'angularx-flatpickr';
-import { CalendarComponent } from './calendar/calendar.component';
-import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { NotasFaltasComponent } from './notas-faltas/notas-faltas.component';
 import { HorarioAulaComponent } from './horario-aula/horario-aula.component';
 import { CalendarioAcademicoComponent } from './calendario-academico/calendario-academico.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -33,7 +31,6 @@ import { CalendarioAcademicoComponent } from './calendario-academico/calendario-
     DashboardComponent,
     AvisoComponent,
     MaterialDidaticoComponent,
-    CalendarComponent,
     NotasFaltasComponent,
     HorarioAulaComponent,
     CalendarioAcademicoComponent
@@ -43,13 +40,13 @@ import { CalendarioAcademicoComponent } from './calendario-academico/calendario-
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    NgbModalModule,
-    FlatpickrModule.forRoot(),
+    BrowserAnimationsModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
     })
   ],
+  exports: [CalendarioAcademicoComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/calendario-academico/calendario-academico.component.ts
+++ src/app/calendario-academico/calendario-academico.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CalendarEvent } from 'calendar-utils';
+import { Subject } from 'rxjs';
+import { colors } from '../model/colors';
 
 @Component({
   selector: 'app-calendario-academico',
@@ -7,6 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarioAcademicoComponent implements OnInit {
 
+  @Input() view: string = 'month';
+
+  @Input() viewDate: Date = new Date();
+
+  @Input() locale: string = 'en';
+
+  @Output() viewChange: EventEmitter<string> = new EventEmitter();
+
+  @Output() viewDateChange: EventEmitter<Date> = new EventEmitter();
+
+
+  events: CalendarEvent[] = [
+
+    {start: new Date(), title: 'Teste'},
+    {start: new Date(), title: 'Teste2'}
+
+  ];
+
+  refresh: Subject<any> = new Subject();
+
+  addEvent(date: Date): void {
+    this.events.push({
+      start: date,
+      title: 'New event',
+      color: colors.red
+    });
+    this.refresh.next();
+  }
   constructor() { }
 
   ngOnInit() {
