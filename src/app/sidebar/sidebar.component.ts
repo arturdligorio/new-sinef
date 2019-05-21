3b10f1cd8d93345d0b3ecd7141435ebda0cb82a0
wagmiranda23 <47559562+wagmiranda23@users.noreply.github.com>
2019-05-21 00:53:50 -0300

    alter-master

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,65 +10,44 @@ export class SidebarComponent implements OnInit {
 
   icons: SidebarStruct [] = [
     {
-      "path": "dashboard" ,
-      "icon": "home",
-      "name": "Área do aluno",
-      "profile":"aluno" 
-  
+      path: "dashboard" ,
+      icon: "home",
+      name: "Área do aluno",
+      profile:"aluno"
     },
     {
-      "path": "horario-aula" ,
-      "icon": "access_time",
-      "name": "Horário de Aula",
-      "profile":"aluno" 
+      path: "horario-aula" ,
+      icon: "access_time",
+      name: "Horário de Aula",
+      profile:"aluno" 
     },
     {
-      "path": "material-didatico" ,
-      "icon": "book",
-      "name": "Material Didático",
-      "profile":"aluno" 
+      path: "material-didatico" ,
+      icon: "book",
+      name: "Material Didático",
+      profile:"aluno" 
     },
     {
-      "path": "notas-faltas" ,
-      "icon": "assessment",
-      "name": "Notas e Faltas",
-      "profile":"aluno"
+      path: "notas-faltas" ,
+      icon: "assessment",
+      name: "Notas e Faltas",
+      profile:"aluno"
     },
     {
-      "path": "calendario-academico" ,
-      "icon": "calendar_today",
-      "name": "Calendário Acadêmico",
-      "profile":"aluno"
-    },
-    //-------------------------------
-    {
-      "path": "dashboard" ,
-      "icon": "home",
-      "name": "Área do professor",
-      "profile":"professor"
+      path: "calendario-academico" ,
+      icon: "calendar_today",
+      name: "Calendário Acadêmico",
+      profile:"aluno"
     },
     {
-      "path": "horario-professor" ,
-      "icon": "access_time",
-      "name": "Horário de Aula",
-      "profile":"professor"
-    },
-    {
-      "path": "notas-faltas" ,
-      "icon": "home",
-      "name": "Notas e Faltas",
-      "profile":"professor"
-    },
-    {
-      "path": "notas-faltas" ,
-      "icon": "home",
-      "name": "Notas e Faltas",
-      "profile":"professor"
+      path: "aviso" ,
+      icon: "list",
+      name: "Avisos",
+      profile:"aluno"
     }
 
   ]
-    
-  
+
   constructor() { }
 
   ngOnInit() {
